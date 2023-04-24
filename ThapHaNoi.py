06e99f2fe80884ae8fb1a93c3e949c32c4e4ffa9
TLTT245 <115201391+TLTT245@users.noreply.github.com>
2023-04-24 10:38:06 +0700

    Delete ThapHaNoi.py

--- ThapHaNoi.py
+++
@@ -1,9 +0,0 @@
-def dichuyen(fp, tp):
-    print("Di chuyển từ cọc",fp, 'tới cọc', tp)
-def thapHanoi(n, a, b, c):
-    if n > 0:
-        thapHanoi(n-1, a, c, b)
-        dichuyen(a, c)
-        thapHanoi(n-1 ,b, a, c)
-n = int(input('Nhập n vào từ bàn phím ='))    
-thapHanoi(n, "A", "B", "C")
